ce815a0c9b247390f4c6fde08af8227a1b8c8958
Rafael Santos <rafaelsaannttos@gmail.com>
2023-08-16 22:02:51 -0300

    feat: operaçoes

---
+++ operacoes.php
@@ -0,0 +1,19 @@
+<?php 
+
+$idade = 21; 
+
+$idadeDaquiDezAnos = $idade + 10; 
+$idadeHa5Anos = $idade - 5; 
+
+$multiplicacao = 3 * 3; 
+$divisao = 15 / 3; 
+$potencia = 2 ** 3; 
+$resto = 12 % 5; 
+
+echo $multiplicacao;
+echo $divisao;
+echo $resto;
+
+echo $idadeDaquiDezAnos;
+echo $idadeHa5Anos;
+
