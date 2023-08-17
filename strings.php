01ba1b195ebff869008e16f74173675a733b21e3
Rafael Santos <rafaelsaannttos@gmail.com>
2023-08-16 22:54:30 -0300

    feat: php basic comands

---
+++ strings.php
@@ -0,0 +1,17 @@
+<?php 
+
+$nome = "Rafael";
+$idade = 21;
+
+// Aspas simples não interpreta vai, aspas duplas sim
+
+// concatenação em php
+echo 'Minha idade é ' . $idade . ' anos e meu nome é ' . $nome . PHP_EOL;
+
+//Quebra de linha
+echo "\n";
+
+// PHP_EOL End of line 
+
+//template string
+echo "\t Minha idade é $idade anos e meu nome é $nome ";
---
+++ tipos.php
@@ -0,0 +1,15 @@
+<?php 
+
+$salario = 3500.50;
+
+// Double ou float 
+$teste = 10 / 3;
+
+// String 
+$nome = "Rafael";
+
+// Boolean
+$verdadeiro = true;
+$falso = false;
+
+echo gettype($verdadeiro);
